f983c9ecb52366732aba63433621c823eba593bf
linfut <37005873+TheOpenSuite@users.noreply.github.com>
2025-09-29 14:54:54 +0300

    Edited logging sink

--- modules/logging-sinks/main.tf
+++ modules/logging-sinks/main.tf
@@ -7,11 +7,11 @@ resource "google_logging_project_sink" "sink" {
 }
 
 resource "google_project_iam_member" "log_writer" {
-  for_each = google_logging_project_sink.sink.writer_identity != "" ? { "sink" = google_logging_project_sink.sink } : {}
-
   project = var.log_destination_project_id
   role    = "roles/logging.logBucketWriter"
-  member  = each.value.writer_identity
+  member  = google_logging_project_sink.sink.writer_identity
 
   depends_on = [google_logging_project_sink.sink]
 }
+
+
